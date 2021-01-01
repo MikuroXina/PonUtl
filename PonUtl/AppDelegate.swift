29954cd9d773f6e4466eef32f263ae75b7fb1cda
MikuroXina <ryosukadnak@gmail.com>
2021-01-01 19:25:28 +0900

    Initial Commit

---
+++ PonUtl/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  PonUtl
+//
+//  Created by Mikuro Xina on 2021/01/01.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PonUtl/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PonUtl
+//
+//  Created by Mikuro Xina on 2021/01/01.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
